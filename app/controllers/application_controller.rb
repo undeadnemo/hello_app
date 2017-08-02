ee6eecae9b902b0f0e0d0f7c09800d74828a3453
undeadnemo <undeadnemo@126.com>
2017-08-02 23:08:37 +0800

    finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-  	render html:" ¡ hello world!"
+  	render html:" hello world!"
   end
-  def goodbye
-  	render html:"goodbye world!"
-  end
+   
 end
